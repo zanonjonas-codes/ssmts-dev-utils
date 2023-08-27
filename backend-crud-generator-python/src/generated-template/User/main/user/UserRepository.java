f41c068011a8c9e67eec249220753d13edc7a2ea
Jonas Zanon <zanonjonas.codes@gmail.com>
2023-08-27 01:40:30 -0300

    git ignoring

--- backend-crud-generator-python/src/generated-template/User/main/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zanonjonascodes.ssmts.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-  
-}
